57da35721569ce7898e7bb36d182661c56df57ff
radish <radishxtop@gmail.com>
2017-11-13 22:51:07 +0800

    add support to ctrl

--- key-bind.js
+++ key-bind.js
@@ -1,7 +1,18 @@
+//cat map.txt|awk '{printf "key_map[\"%s\"]=\"%s\";\n",$2,$1}'
+// 猴子重复工作50分钟，人用45分钟发明工具，工作5分钟
+c_map = {
+	'a':"() => {alert(213);}",
+	'v':"() => {window.scrollBy(0,100);}"
+}
+map = {}
+
+
+
 $(document).keypress(function(e) {
-	alert(e.which)
-  if(e.which == 13) {
-    // enter pressed
-    alert("aaa");
-  }
+	if(e.ctrlKey){
+		eval(c_map[String.fromCharCode(e.which+96)])();
+	}
+	console.log("-------------")
+	console.log(e.which)
+	console.log(String.fromCharCode(e.which))
 });
